ed38434c55e09ee8183ef190bcbe23baee27a993
Ashish Sah <52920044+Ashish-sah@users.noreply.github.com>
2020-12-05 14:37:54 +0530

    file to store current user using Paper library

---
+++ app/src/main/java/com/ashish/ecommerceapp/Prevalent/Prevalent.java
@@ -0,0 +1,11 @@
+package com.ashish.ecommerceapp.Prevalent;
+
+import com.ashish.ecommerceapp.Model.Users;
+
+//Used for the current user and see which is active or not using Paper library
+public class Prevalent {
+    public static final String UserPhoneKey = "UserPhone";
+    public static final String UserPasswordKey = "UserPassword";
+    public static Users currentOnlineUser;
+
+}
