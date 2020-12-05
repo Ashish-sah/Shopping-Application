ef42428796473cc32670425bf09e3da8c96907d4
Ashish Sah <52920044+Ashish-sah@users.noreply.github.com>
2020-12-05 14:44:59 +0530

    Login Portal

---
+++ app/src/main/java/com/ashish/ecommerceapp/LoginActivity.java
@@ -0,0 +1,149 @@
+package com.ashish.ecommerceapp;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.ashish.ecommerceapp.Model.Users;
+import com.ashish.ecommerceapp.Prevalent.Prevalent;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.rey.material.widget.Button;
+import com.rey.material.widget.CheckBox;
+import com.rey.material.widget.EditText;
+
+import io.paperdb.Paper;
+
+public class LoginActivity extends AppCompatActivity {
+    FirebaseDatabase database;
+    DatabaseReference databaseReference;
+    private EditText InputPhoneNumber, InputPassword;
+    private Button LoginButton;
+    private ProgressDialog loadingBar;
+    private TextView AdminLink, NotAdminLink;
+    private String parentDbName = "Users";
+    private CheckBox chkBoxRememberMe;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        LoginButton = (Button) findViewById(R.id.login_btn);
+        InputPassword = (EditText) findViewById(R.id.login_password_input);
+        InputPhoneNumber = (EditText) findViewById(R.id.login_phone_number_input);
+        AdminLink = (TextView) findViewById(R.id.admin_panel_link);
+        NotAdminLink = (TextView) findViewById(R.id.not_admin_panel_link);
+        loadingBar = new ProgressDialog(this);
+        chkBoxRememberMe = (CheckBox) findViewById(R.id.remember_me_chkb);
+        //This library write the user information to android memory
+        Paper.init(this);
+        database = FirebaseDatabase.getInstance();
+        databaseReference = database.getReference();
+        LoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LoginUser();
+            }
+        });
+
+        AdminLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LoginButton.setText("Login Admin");
+                AdminLink.setVisibility(View.INVISIBLE);
+                NotAdminLink.setVisibility(View.VISIBLE);
+                parentDbName = "Admins";
+                chkBoxRememberMe.setEnabled(false);
+            }
+        });
+
+        NotAdminLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LoginButton.setText("Login ");
+                AdminLink.setVisibility(View.VISIBLE);
+                NotAdminLink.setVisibility(View.INVISIBLE);
+                chkBoxRememberMe.setEnabled(true);
+                parentDbName = "Users";
+            }
+        });
+    }
+
+    private void LoginUser() {
+        String phone = InputPhoneNumber.getText().toString().trim();
+        String password = InputPassword.getText().toString().trim();
+
+        if (TextUtils.isEmpty(phone)) {
+            Toast.makeText(this, "Please write your phone number...", Toast.LENGTH_SHORT).show();
+        } else if (TextUtils.isEmpty(password)) {
+            Toast.makeText(this, "Please write your password...", Toast.LENGTH_SHORT).show();
+        } else {
+            loadingBar.setTitle("Login Account");
+            loadingBar.setMessage("Please wait, while we are checking the credentials.");
+            loadingBar.setCanceledOnTouchOutside(false);
+            loadingBar.show();
+            AllowAccessToAccount(phone, password);
+        }
+    }
+
+    private void AllowAccessToAccount(String phone, String password) {
+        //storing the value inside prevalent variables if check box is checked
+        if (chkBoxRememberMe.isChecked()) {
+            Paper.book().write(Prevalent.UserPhoneKey, phone);
+            Paper.book().write(Prevalent.UserPasswordKey, password);
+        }
+
+        databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.child(parentDbName).child(phone).exists()) {
+                    Users userData = snapshot.child(parentDbName).child(phone).getValue(Users.class);
+                    assert userData != null;
+                    if (userData.getPhone().equals(phone)) {
+                        if (userData.getPassword().equals(password)) {
+                            if (parentDbName.equals("Admins")) {
+                                Toast.makeText(getApplicationContext(), "Welcome Admin you are Logged In Successfully", Toast.LENGTH_SHORT).show();
+                                loadingBar.dismiss();
+                                Intent inten = new Intent(LoginActivity.this, Admin_Category_Activity.class);
+                                startActivity(inten);
+                                finish();
+                            } else if (parentDbName.equals("Users")) {
+                                Toast.makeText(getApplicationContext(), "Logged In Successfully", Toast.LENGTH_SHORT).show();
+                                loadingBar.dismiss();
+                                Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
+                                Prevalent.currentOnlineUser = userData;
+                                startActivity(intent);
+                                finish();
+                            } else {
+                                loadingBar.dismiss();
+                                Toast.makeText(getApplicationContext(), "DETAILS INCORRECT number :" + phone
+                                        + " password = " + password, Toast.LENGTH_LONG).show();
+                            }
+                        } else {
+                            loadingBar.dismiss();
+                            Toast.makeText(getApplicationContext(), "Password is incorrect", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                } else {
+                    Toast.makeText(getApplicationContext(), "Account with this " + phone + " doesn't exist ", Toast.LENGTH_SHORT).show();
+                    loadingBar.dismiss();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+}
