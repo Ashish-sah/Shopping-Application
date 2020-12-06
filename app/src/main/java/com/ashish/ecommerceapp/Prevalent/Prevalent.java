ca46c82dab4e17ccb7bbb023e9e555a221acc3d3
Ashish Sah <52920044+Ashish-sah@users.noreply.github.com>
2020-12-06 12:18:44 +0530

    Prevalent Class Modified

--- app/src/main/java/com/ashish/ecommerceapp/Prevalent/Prevalent.java
+++ app/src/main/java/com/ashish/ecommerceapp/Prevalent/Prevalent.java
@@ -4,8 +4,9 @@ import com.ashish.ecommerceapp.Model.Users;
 
 //Used for the current user and see which is active or not using Paper library
 public class Prevalent {
+    public static Users currentOnlineUser;
+
     public static final String UserPhoneKey = "UserPhone";
     public static final String UserPasswordKey = "UserPassword";
-    public static Users currentOnlineUser;
 
 }
